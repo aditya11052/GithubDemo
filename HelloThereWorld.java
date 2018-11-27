ea8095f3436c03e266aae8400677abca4181f977
aditya11052 <40637578+aditya11052@users.noreply.github.com>
2018-11-27 12:12:00 -0500

    Update HelloThereWorld.java
    NNNNNNNNNNNNOICE

--- HelloThereWorld.java
+++ HelloThereWorld.java
@@ -2,6 +2,8 @@ public class HelloThereWorld{
 	public static void main (String[]args){
 
 	System.out.println("Salutations Universe");
+	System.out.println("Whatup Galaxy");
+	System.out.println("Cheers World!");
 	
 	}
 }
